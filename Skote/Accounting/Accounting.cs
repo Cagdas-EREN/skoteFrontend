404fb7b9f5d451e95105b8c60398e378f11ae9d1
Cagdas-EREN <77439910+Cagdas-EREN@users.noreply.github.com>
2023-02-28 10:22:04 +0300

    Add project files.

---
+++ Skote/Accounting/Accounting.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skote.Accounting
+{
+    public class Accounting : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult OdenenFaturalar()
+        {
+            return View();
+        }
+        public IActionResult OdenmeyenFaturalar()
+        {
+            return View();
+        }
+        public IActionResult IptalFaturalar()
+        {
+            return View();
+        }
+        public IActionResult ResmiFaturalar()
+        {
+            return View();
+        }
+        public IActionResult ResmiOlmayanFaturalar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Skote/Orders/Order.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skote.Orders
+{
+    public class Order : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult AktifSiparisler()
+        {
+            return View();
+        }
+        public IActionResult IslemdekiSiparisler()
+        {
+            return View();
+        }
+        public IActionResult AskıyaAlınanSiparisler()
+        {
+            return View();
+        }
+        public IActionResult IptalEdilenSiparisler()
+        {
+            return View();
+        }
+    }
+}
