a4c424bcb2d865ad1fc64a2e80a532975ea7f29f
Cagdas-EREN <77439910+Cagdas-EREN@users.noreply.github.com>
2023-02-28 16:46:49 +0300

    first commit

---
+++ Skote/Customer/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skote.Customer
+{
+    public class Customer : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
